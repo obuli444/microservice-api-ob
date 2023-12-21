03cbdfbd96de58c3c227ced888c6e76090bcd204
Obuliraj V <obuliraj.v@clearcode-labs.com>
2023-12-21 20:45:04 +0530

    router changes added

--- apps/api-auth/src/app/routes.ts
+++ apps/api-auth/src/app/routes.ts
@@ -2,9 +2,5 @@ import { Application } from 'express';
 import Signin from './controllers/signin';
 
 export default function bindRoutes(app: Application) {
-app.get('/test', (req, res) => {
-    console.log("called endpoint");
-        res.send({ message: 'Auth Api works!' });
-});
-app.post('/signin',Signin)
+app.post('/api/v1/signin',Signin)
 }
