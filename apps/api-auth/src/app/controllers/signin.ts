3d13c2039610cd1d13ec4b9954494d80c08f6784
Obuliraj V <obuliraj.v@clearcode-labs.com>
2023-12-21 13:08:19 +0530

    login controller changes added

--- apps/api-auth/src/app/controllers/signin.ts
+++ apps/api-auth/src/app/controllers/signin.ts
@@ -1,9 +1,46 @@
 import { AppRequest, AppResponse } from "@ccl-dopz-api/app-interface";
 import axios from "axios";
+const querystring = require('querystring');
+
 export default async function Signin(req: AppRequest, res: AppResponse) {
-    const parameters  = req.body;
-    console.log("pa",parameters);
+const {client_id,client_secret,username,password,realm} = req.body;
+const keycloakBaseUrl = `https://authenticate.ccapps.in/auth/realms/${realm}/protocol/openid-connect/token`;
+const data = {
+  grant_type: 'password',
+  client_id,
+  client_secret,
+  username: username,
+  password: password,
+};
+
+// Convert data to URL-encoded form
+const encodedData = querystring.stringify(data);
+
+// Set the headers for URL-encoded form data
+const headers = {
+  'Content-Type': 'application/x-www-form-urlencoded'
+};
+axios.post(`${keycloakBaseUrl}`, encodedData, { headers })
+  .then((response)=>{
+    const data  = response.data;
     res.json({
-        status: true
+        status : true,
+        message : 'login success',
+        data : data
     })
+  })
+  .catch(error => {
+    const errordata = error.response ? error.response.data : error.message;
+    res.json({
+        status : false,
+        message : 'login false',
+        data : errordata
+    })
+    req.logger.log(__filename, 'warn',errordata);
+    console.error('Error:',errordata);
+  });
+
+
+
+    
 }
